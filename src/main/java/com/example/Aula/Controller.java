376c782ad5c7ed7003f7b7e79cd86dbc743af20a
GameCrasher <andrelwolski@hotmail.com>
2022-10-25 20:24:43 -0300

    commit aula 2 h2

--- src/main/java/com/example/Aula/Controller.java
+++ src/main/java/com/example/Aula/Controller.java
@@ -1,26 +1,43 @@
 package com.example.Aula;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
 
-import java.lang.ref.Cleaner;
+import com.example.Aula.Cliente;
+import com.example.Aula.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/cliente/v1/")
+@RequestMapping({"/cliente/v1"})
 public class Controller {
-
-
     @Autowired
     Repository repository;
 
+    public Controller() {
+    }
 
     @PostMapping
-    public Cliente create(@RequestBody Cliente cliente){
-        Cliente clienteSaved = repository.save(cliente);
+    public Cliente creat(@RequestBody Cliente cliente) {
+        Cliente clienteSaved = (Cliente)this.repository.save(cliente);
         return clienteSaved;
+    }
+
+    @GetMapping({"/{id}"})
+    @ResponseBody
+    public Optional<Cliente> getClineteById(@PathVariable Long id) {
+        Optional<Cliente> clienteReturned = this.repository.findById(id);
+        return clienteReturned;
+    }
+
+    @DeleteMapping({"/{id}"})
+    public void deleteClienteById(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+    @GetMapping
+    public List<Cliente> listClientes(){
+        return repository.findAll();
     }
 
 }
